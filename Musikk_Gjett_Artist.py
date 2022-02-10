d2d838c56244fe3fecd9062555df325f1b2b4a64
TorGj <82477510+TorGj@users.noreply.github.com>
2022-02-10 11:35:10 +0100

    Diverse små endringer

--- Musikk_Gjett_Artist.py
+++ Musikk_Gjett_Artist.py
@@ -1,11 +1,37 @@
 from playsound import playsound
+from pygame import mixer
+import time
+import os, random
 
-def ps():
-    playsound('musikk/sail.mp3', block=False)
+poeng = 0
 
-ps()
+def velg_artist():
+    valg = random.choice(os.listdir("musikk"))
+    while valg == '.DS_Store':
+        valg = random.choice(os.listdir("musikk"))
+    artist = valg.split("_")
+    artist = artist[0]
+    #print(valg)
+    return valg, artist
+
+def ps(sang):
+    mixer.init()  # Initialzing pyamge mixer
+    mixer.music.stop()
+    mixer.music.load('musikk/%s' %sang)  # Loading Music File
+    mixer.music.play()  # Playing Music with Pygame
+    #time.sleep(5)
+    #print('musikk/%s' % sang)
 
 while True:
-    stopp = input('Stopp?')
-    if stopp =='y':
+    valgt = velg_artist()
+    print(valgt)
+    ps(valgt[0])
+    stopp = input('Hvem er dette?')
+
+    print(valgt[1])
+    if stopp == valgt[1]:
+        poeng = poeng + 1
+
+    if poeng > 2:
         break
+
