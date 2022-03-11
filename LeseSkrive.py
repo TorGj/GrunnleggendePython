b80cba7196adc02e0807a2210563465f0ca202a4
TorGj <82477510+TorGj@users.noreply.github.com>
2022-03-11 08:26:51 +0100

    Diverse små endringer

--- LeseSkrive.py
+++ LeseSkrive.py
@@ -1,5 +1,5 @@
-def lagre(resultat, bruker):
-    with open('%s.txt' % bruker, 'w') as f:
+def lagre(resultat, bruker, alt):
+    with open('%s.txt' % bruker, '%s' % alt) as f:
         f.writelines(str(resultat))
         f.write('\n')
     f.close()           # Close file to allow other to access file
@@ -10,3 +10,10 @@ def les_fil(bruker):
     innhold = min_fil.read()
     min_fil.close()     # Close file to allow other to access file
     return innhold
+
+tilfil = str(input('Skriv det som skal lagres:'))
+lagre(tilfil,'Tor', 'w')
+
+lestfil = les_fil('Tor')
+print(lestfil)
+
